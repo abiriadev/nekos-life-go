d60c917897738b112025925a0edc9f3829860c95
Abiria <abiria.dev@gmail.com>
2024-01-03 14:47:56 +0900

    feat: provide public apis

---
+++ lib.go
@@ -0,0 +1,31 @@
+package nekoslifego
+
+func Tickle() string     { return "" }
+func Slap() string       { return "" }
+func Poke() string       { return "" }
+func Pat() string        { return "" }
+func Neko() string       { return "" }
+func Meow() string       { return "" }
+func Lizard() string     { return "" }
+func Kiss() string       { return "" }
+func Hug() string        { return "" }
+func FoxGirl() string    { return "" }
+func Feed() string       { return "" }
+func Cuddle() string     { return "" }
+func Why() string        { return "" }
+func CatText() string    { return "" }
+func OwOify() string     { return "" }
+func EightBall() string  { return "" }
+func Fact() string       { return "" }
+func NekoGif() string    { return "" }
+func Kemonomimi() string { return "" }
+func Holo() string       { return "" }
+func Smug() string       { return "" }
+func Baka() string       { return "" }
+func Woof() string       { return "" }
+func Spoiler() string    { return "" }
+func Wallpaper() string  { return "" }
+func Goose() string      { return "" }
+func Gecg() string       { return "" }
+func Avatar() string     { return "" }
+func waifu() string      { return "" }
